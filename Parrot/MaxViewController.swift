675492784cc0fa99daac0a2e7a82166cd533b3a0
Ariven <ariven@Arivens-Mac.local>
2017-08-25 22:29:45 +0530

    MaxViewController Added

---
+++ Parrot/MaxViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MaxViewController.swift
+//  Parrot
+//
+//  Created by Ariven on 25/08/17.
+//  Copyright © 2017 Ariven. All rights reserved.
+//
+
+import UIKit
+
+class MaxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("Parrots are sometime red yellow and green")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
