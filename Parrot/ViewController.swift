7bccdfc2a5f48d48cebc1dc17548d9891d2d8e84
Ariven <ariven@Arivens-Mac.local>
2017-08-25 22:39:04 +0530

    second commit

--- Parrot/ViewController.swift
+++ Parrot/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("for first commit")
+        
+        print("for second commit")
     }
 
     override func didReceiveMemoryWarning() {
